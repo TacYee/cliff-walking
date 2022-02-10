7d483f0d88c9dd81d0e83ad714be3ba32628e6e9
TacYee <72900874+TacYee@users.noreply.github.com>
2022-02-10 13:14:49 +0800

    Create TD.py

---
+++ TD.py
@@ -0,0 +1,60 @@
+'''
+@author:Chaoxiang  Ye 
+    11.03.2021
+
+'''
+
+from cliff import next_state_reward_end
+from table import q_table
+from action import epsilon_action, greedy_action
+
+
+# Discount parameter
+lam=1
+
+# learning rate
+lr=0.1
+
+
+def q_learning(state,epsilon):
+    # epsilon-greedy choose the action of the current state
+    action, q = epsilon_action(state,epsilon)
+    
+    # next state. reward, and end flag
+    next_state, reward, end_flag=next_state_reward_end(state, action)   
+
+    # evaluation next action of next state
+    next_action, next_q=greedy_action(next_state)
+ 
+    #estimate the current action value 
+    estimate=reward+lam*next_q
+    
+    #TD error
+    td_error = estimate - q
+    
+    #update the current action value
+    q_table[(state, action)]+=lr*td_error
+    
+    return next_state, reward, end_flag 
+
+
+def sarsa_learning(state,epsilon):
+    # epsilon-greedy choose the action of the current state(same as q_learning)
+    action, q = epsilon_action(state,epsilon)
+    
+    #  next state. reward, and end flag
+    next_state, reward, end_flag=next_state_reward_end(state, action)   
+
+    #  evaluation next action of next state(different from q_learning)
+    next_action, next_q=epsilon_action(next_state,epsilon)
+ 
+    #estimate the current action value 
+    estimate=reward+lam*next_q
+    
+    #TD error
+    td_error = estimate - q
+    
+    #update the current action value
+    q_table[(state, action)]+=lr*td_error
+    
+    return next_state, reward, end_flag 
